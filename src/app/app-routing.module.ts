c38b26c92873f7716a74ba211ca898428bada112
Ahmet Bakırcı <ahmetbakirci30@gmail.com>
2021-11-14 10:41:38 +0300

    fix(route): fixed page not found on reload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { AppAdsComponent } from './components/app-ads/app-ads.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { VideoListComponent } from './components/video/video-list/video-list.component';
 import { NgModule } from '@angular/core';
@@ -8,7 +7,6 @@ const routes: Routes = [
   { path: '', component: VideoListComponent },
   { path: 'videos', component: VideoListComponent },
   { path: 'videos/category/:categoryId', component: VideoListComponent },
-  { path: 'app-ads.txt', component: AppAdsComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
 
